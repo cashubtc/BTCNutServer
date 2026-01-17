e9cbf0b22687373d81f10f7a96517bfb9d0693e0
d4rp4t <d4rp4t@tutanota.com>
2026-01-18 00:41:59 +0100

    Make CashuUtils thiner (still too thick)

--- Plugin/BTCPayServer.Plugins.Cashu/CashuAbstractions/CashuUtils.cs
+++ Plugin/BTCPayServer.Plugins.Cashu/CashuAbstractions/CashuUtils.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 using BTCPayServer.Lightning;
 using BTCPayServer.Plugins.Cashu.Errors;
@@ -11,9 +10,8 @@ using DotNut;
 using DotNut.Api;
 using DotNut.ApiModels;
 using NBitcoin;
-using NBitcoin.Secp256k1;
-using DLEQProof = DotNut.DLEQProof;
-using DotNut.NUT13;
+using Utils = DotNut.Abstractions.Utils;
+
 namespace BTCPayServer.Plugins.Cashu.CashuAbstractions;
 
 public static class CashuUtils
@@ -107,33 +105,7 @@ public static class CashuUtils
             Unit = token.Unit ?? "sat"
         };
     }
-
-    /// <summary>
-    /// Function mapping payment amount to keyset supported amounts in order to create swap payload. Always tries to fit the biggest proof.
-    /// </summary>
-    /// <param name="paymentAmount">Amount that has to be covered.</param>
-    /// <param name="keyset">Mints keyset></param>
-    /// <returns>List of ulong proof amounts for given keyset</returns>
-    public static List<ulong> SplitToProofsAmounts(ulong paymentAmount, Keyset keyset)
-    {
-        var outputAmounts = new List<ulong>();
-        var possibleValues = keyset.Keys.OrderByDescending(x => x).ToList();
-        foreach (var value in possibleValues)
-        {
-            while (paymentAmount >= value)
-            {
-                outputAmounts.Add(value);
-                paymentAmount -= value;
-            }
-
-            if (paymentAmount == 0)
-            {
-                break;
-            }
-        }
-
-        return outputAmounts;
-    }
+    
 
     /// <summary>
     /// Function selecting proofs to send and to keep from provided inputAmounts. 
@@ -157,13 +129,13 @@ public static class CashuUtils
         }
 
         var change = inputAmounts.Aggregate((a, c) => a + c) - requestedAmont;
-        var sendAmounts = SplitToProofsAmounts(requestedAmont, keyset);
+        var sendAmounts = Utils.SplitToProofsAmounts(requestedAmont, keyset);
         if (change == 0)
         {
             return (new List<ulong>(), sendAmounts);
         }
 
-        var keepAmounts = SplitToProofsAmounts(change, keyset);
+        var keepAmounts = Utils.SplitToProofsAmounts(change, keyset);
 
         return (keepAmounts, sendAmounts);
     }
@@ -177,16 +149,9 @@ public static class CashuUtils
     /// <returns>Blank Outputs</returns>
     public static OutputData CreateBlankOutputs(ulong amount, KeysetId keysetId, Keyset keys, DotNut.NBitcoin.BIP39.Mnemonic? mnemonic = null, uint? counter = null)
     {
-        if (amount == 0)
-        {
-            throw new ArgumentException("Cannot create blank outputs zero amount.");
-        }
-
-        var count = CalculateNumberOfBlankOutputs(amount);
-
-        // Amount is set for 1, they're blank. Mint will automatically set their amount and sign each by pk corresponding to value
-        var amounts = Enumerable.Repeat((ulong)1, count).ToList();
-        return CreateOutputs(amounts, keysetId, keys, mnemonic, counter);
+        // todo migrate to new outputdata format, since this one is retarded
+        var outputs = Utils.CreateBlankOutputs(amount, keysetId, keys, mnemonic, counter);
+        return ConvertOutputData(outputs);
     }
 
     /// <summary>
@@ -195,6 +160,7 @@ public static class CashuUtils
     /// <param name="amounts">Amounts for each output (e.g. [1,2,4,8]</param>
     /// <param name="keysetId">ID of keyset we want to receive the proofs</param>
     /// <param name="keys">Keyset for given ID</param>
+    /// <param
     /// <returns></returns>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
     public static OutputData CreateOutputs(
@@ -204,134 +170,20 @@ public static class CashuUtils
         DotNut.NBitcoin.BIP39.Mnemonic? mnemonic = null,
         uint? counter = null)
     {
-        if (amounts.Any(a => !keys.Keys.Contains(a)))
-            throw new ArgumentException("Invalid amounts");
+        var outputs = DotNut.Abstractions.Utils.CreateOutputs(amounts, keysetId, keys, mnemonic, counter);
+        return ConvertOutputData(outputs);
+    }
 
-        var blindedMessages = new List<BlindedMessage>(amounts.Count);
-        var secrets = new List<DotNut.ISecret>(amounts.Count);
-        var blindingFactors = new List<PrivKey>(amounts.Count);
-
-        Func<DotNut.ISecret> secretFactory;
-        Func<PrivKey> blindingFactorFactory;
-
-        if (mnemonic is not null && counter is { } c)
+    private static OutputData ConvertOutputData(List<DotNut.Abstractions.OutputData> outputDataList)
+    {
+        return new OutputData
         {
-            secretFactory = () => mnemonic.DeriveSecret(keysetId, c);
-            blindingFactorFactory = () => new PrivKey(
-                Convert.ToHexString(mnemonic.DeriveBlindingFactor(keysetId, c))
-            );
-        }
-        else
-        {
-            secretFactory = () =>
-            {
-                var bytes = RandomNumberGenerator.GetBytes(32);
-                return new StringSecret(Convert.ToHexString(bytes));
-            };
-
-            blindingFactorFactory = () =>
-            {
-                var bytes = RandomNumberGenerator.GetBytes(32);
-                return new PrivKey(Convert.ToHexString(bytes));
-            };
-        }
-
-        foreach (var amount in amounts)
-        {
-            var secret = secretFactory();
-            secrets.Add(secret);
-
-            var r = blindingFactorFactory();
-            blindingFactors.Add(r);
-
-            var B_ = DotNut.Cashu.ComputeB_(secret.ToCurve(), r);
-            blindedMessages.Add(new BlindedMessage() { Amount = amount, B_ = B_, Id = keysetId });
-        }
-
-        return new OutputData()
-        {
-            BlindingFactors = blindingFactors.ToArray(),
-            BlindedMessages = blindedMessages.ToArray(),
-            Secrets = secrets.ToArray()
+            BlindedMessages = outputDataList.Select(od => od.BlindedMessage).ToArray(),
+            Secrets = outputDataList.Select(od => od.Secret).ToArray(),
+            BlindingFactors = outputDataList.Select(od => od.BlindingFactor).ToArray()
         };
     }
-
-
-
-    /// <summary>
-    /// Calculates amount of blank outputs needed by mint to return overpaid fees
-    /// </summary>
-    /// <param name="amountToCover">Amount of tokens that has to be covered by mint.</param>
-    /// <returns>Integer amount of blank outputs needed</returns>
-    /// <exception cref="Exception">If amount is 0 - idk why someone would do that</exception>
-    private static int CalculateNumberOfBlankOutputs(ulong amountToCover)
-    {
-        if (amountToCover == 0)
-        {
-            return 0;
-        }
-
-        return Math.Max(
-            Convert.ToInt32(
-                Math.Ceiling(
-                    Math.Log2(amountToCover)
-                )
-            ), 1);
-    }
-
-    /// <summary>
-    /// Removes dleq from proof.
-    /// </summary>
-    /// <param name="proofs"></param>
-    /// <returns>proof without dleq</returns>
-    public static List<Proof> StripDleq(List<Proof> proofs)
-    {
-        foreach (var proof in proofs)
-        {
-            proof.DLEQ = null;
-        }
-
-        return proofs;
-    }
-
-    /// <summary>
-    ///  Method creating proofs, from provided promises (blinded signatures)
-    /// </summary>
-    /// <param name="promise">Blinded Signature</param>
-    /// <param name="r">Blinding factor</param>
-    /// <param name="secret">Yeah, secret</param>
-    /// <param name="amountPubkey">Key, corresponding to proof amount</param>
-    /// <returns>Valid proof</returns>
-    private static Proof ConstructProofFromPromise(
-        BlindSignature promise,
-        ECPrivKey r,
-        DotNut.ISecret secret,
-        ECPubKey amountPubkey)
-    {
-
-        //unblind signature
-        var C = DotNut.Cashu.ComputeC(promise.C_, r, amountPubkey);
-
-        if (promise.DLEQ is not null)
-        {
-            promise.DLEQ = new DLEQProof
-            {
-                E = promise.DLEQ.E,
-                S = promise.DLEQ.S,
-                R = r
-            };
-        }
-
-        return new Proof
-        {
-            Id = promise.Id,
-            Amount = promise.Amount,
-            Secret = secret,
-            C = C,
-            DLEQ = promise.DLEQ,
-        };
-    }
-
+    
     /// <summary>
     /// Create Proofs from BlindSignature array
     /// </summary>
@@ -363,9 +215,12 @@ public static class CashuUtils
             var r = rs[i];
             var secret = secrets[i];
 
-            var A = keyset[Convert.ToUInt64(p.Amount)];
+            if (!keyset.TryGetValue(Convert.ToUInt64(p.Amount), out var A))
+            {
+                 throw new CashuPluginException($"Provided keyset doesn't contain PubKey for amount {p.Amount}");
+            }
 
-            proofs.Add(ConstructProofFromPromise(p, r, secret, A));
+            proofs.Add(DotNut.Abstractions.Utils.ConstructProofFromPromise(p, r, secret, A));
         }
 
         return proofs.ToArray();
@@ -511,13 +366,6 @@ public static class CashuUtils
         public string Unit { get; set; }
 
         public ulong SumProofs => Proofs?.Select(p => p.Amount).Sum() ?? 0;
-    }
-
-    public class SendResponse
-    {
-        public List<Proof> Keep { get; set; }
-
-        public List<Proof> Send { get; set; }
     }
 
     public class OutputData
